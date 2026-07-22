1f6c47352c621c3dad4b1a73616ae7db1033b982
hadi-mapper <hadi.wibowo557@gmail.com>
2026-07-22 17:08:13 +0700

    Create Home page structure

---
+++ website/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+function Home() {
+  return (
+    <div>
+      <h1>Hadi Wibowo</h1>
+
+      <h2>
+        GIS Professional Portfolio
+      </h2>
+
+      <p>
+        Mine Survey | GIS | Drone Mapping | Remote Sensing
+      </p>
+
+      <p>
+        Welcome to my spatial journey.
+      </p>
+    </div>
+  )
+}
+
+export default Home
