198e78f0eeb4699c54ac3e7b3df64bb22fd87428
hadi-mapper <hadi.wibowo557@gmail.com>
2026-07-22 17:11:56 +0700

    Add navigation component

---
+++ website/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>Hadi Wibowo</h2>
+
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>WebGIS</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
